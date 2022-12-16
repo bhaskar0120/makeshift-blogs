9ca0afea8cb354586935f7cb2c1b31f44686c3e9
bhaskar0120 <bhaskar0120@gmail.com>
2022-12-16 09:29:36 +0530

    Testing one

---
+++ publisher.py
@@ -0,0 +1,79 @@
+from sys import argv
+from json import dump, load
+from re import sub
+from datetime import datetime
+
+# a function to get the current date in the format DD MMM. YYYY
+def get_date():
+    return datetime.now().strftime("%d %b. %Y")
+
+def main():
+    if len(argv) != 2:
+        print("Usage: python publisher.py <FILE.md>")
+        return
+    
+    # Questionaire
+    print("Enter the following details for the blog:")
+    title = input("Title: ")
+    author = input("Author (Bhaskar Bhardwaj): ")
+    if(author == ""):
+        author = "Bhaskar Bhardwaj"
+    description = input("Description: ")
+    date = get_date()
+    print("")
+    print("Is the following information correct?")
+    print("Title: ", title)
+    print("Author: ", author)
+    print("Date: ", date)
+    print("Description: ", description)
+    print("")
+    print("Enter 'Y' to continue or any other key to exit")
+    if input() != 'Y':
+        return
+    
+    # create id for new blog
+    id = sub(' ', '-', title.lower())
+    # remove special characters
+    id = sub('[^a-z0-9-]', '', id)
+    
+    # create metadata for new blog
+    meta = {
+        "title": title,
+        "id": id,
+        "author": author,
+        "content" : description,
+        "date": date
+    }
+    
+    with open(argv[1]) as f:
+        blog = f.read()
+
+    with open('all.json') as f:
+        all_files = load(f)
+    
+    # increment the total
+    all_files['total'] += 1
+    
+
+    # add the new blog to the list
+    all_files['blogs'].append(meta)
+    
+
+    # write all_files to the file all.json
+    with open('all.json', 'w') as f:
+        dump(all_files, f, indent=4)
+    
+    # create metadata for md file
+    meta_md = {
+        "author": author,
+        "date": date,
+        "blog": blog
+    }
+
+    # write meta_md in blogs/<id>.json
+    with open('./blogs/{}.json'.format(id), 'w') as f:
+        dump(meta_md, f, indent=4)
+
+    
+if __name__ == "__main__":
+    main()
